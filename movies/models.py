427dac84c0e21da2713f70a79f719ef400015fa5
Manjunatha1997 <lovelymanju198@gmail.com>
2020-03-19 14:23:19 +0530

    connected to database

--- movies/models.py
+++ movies/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 # Create your models here.
+#
+# class Movies:
+#     str:'title'
+#     str:'cast'
+#     str:'img'
 
-class Movies:
-    str:'title'
-    str:'cast'
-    str:'img'
-
+class Movies(models.Model):
+    title=models.CharField(max_length=100)
+    cast=models.CharField(max_length=100)
+    img=models.ImageField(upload_to='pics')
